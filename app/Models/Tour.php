8b5ef859f24a43c504d91f811f3dd001413f052c
ASEM ALSALAHI <96667066+ASEMALSALAHI@users.noreply.github.com>
2024-02-12 06:25:50 +0300

    edit_to_Uuid

---
+++ app/Models/Tour.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tour extends Model
+{
+    use HasFactory,HasUuids;
+    protected $fillable = [
+        'travel_id',
+        'name',
+        'strating_date',
+        'ending_date',
+        'price',
+
+    ];
+    public function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100
+        );
+    }
+}
--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Travel extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable,HasUuids ;
     protected $table = 'travels';
     protected $fillable = [
         'is_public',
@@ -16,4 +18,23 @@ class Travel extends Model
         'description',
         'number_of_days',
         ];
+    public function tours()
+    {
+        return $this->hasMany(Tour::class);
+    }
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value,$attribute) => $attribute['number_of_days'] -1
+        );
+    }
 }
