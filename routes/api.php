24453ccd9a102fad072f7785b89e4ea89cde133f
ASEM ALSALAHI <96667066+ASEMALSALAHI@users.noreply.github.com>
2024-07-20 20:18:02 +0300

    Admin create Tour for Travel

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ Route::get('travels/{travel:slug}/tours', [App\Http\Controllers\Api\V1\TourContr
 
 Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function () {
     Route::post('travels', [App\Http\Controllers\Api\V1\Admin\TravelController::class, 'store']);
+    Route::post('travels/{travel}/tours', [App\Http\Controllers\Api\V1\Admin\TourController::class, 'store']);
 });
 
 
